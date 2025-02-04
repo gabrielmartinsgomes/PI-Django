5b5635a1a6a86c2f0173847e47256f1ac5bee959
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2025-02-04 15:16:22 -0300

    Aula 1 - Backend - Nova classe

--- alunos/models.py
+++ alunos/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
-# Create your models here.
 class Estado(models.Model):
     nome = models.CharField(max_length=50)
     sigla = models.CharField(max_length=2)
 
     def __str__(self):
         return f"{self.nome} ({self.sigla})"
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.nome} ({self.estado.sigla})"
+    
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=80)
+    telefone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    nascimento = models.DateField
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome
